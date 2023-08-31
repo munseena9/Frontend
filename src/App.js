4b69f896a96443081c0495034d2383fee783bc18
munseena9 <143597371+munseena9@users.noreply.github.com>
2023-08-31 16:13:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Appbar1 from './components/Appbar';
+import Student from './components/Student';
+
+
+function App() {
+  return (
+    <div className='App'>
+   
+      <Appbar1/>
+      <Student/>
+    </div>
+
+
+   
+  );
+}
+
+
+export default App;
